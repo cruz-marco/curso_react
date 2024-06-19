437f14fb3ceab66e39b844d562e1ceeb72a76309
Marco Cruz <marco_cruz@hotmail.com.br>
2024-06-18 21:58:09 -0300

    Módulo 1 Completo.

---
+++ 01_intro/hello-world/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Olá mundo!</h1>
+      <h2 class="subtitle">Eu gosto de batatas</h2>
+    </div>
+  );
+}
+
+export default App;
