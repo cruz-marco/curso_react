61ece930025a9e60a410e1f9e692a5fa0f5736b5
Marco Cruz <marco_cruz@hotmail.com.br>
2024-06-30 10:53:51 -0300

    Estruturando o componente Game

--- 06_pjSecretWord/secret_word/src/components/Game.jsx
+++ 06_pjSecretWord/secret_word/src/components/Game.jsx
@@ -4,8 +4,29 @@ import React from 'react'
 
 function Game() {
   return (
-    <div>
-        <h1>Game</h1>
+    <div className={styles.game}>
+      <p className={styles.points}>
+        <span>Pontuação: 0000</span>
+      </p>
+      <h1>Advinhe a palavra:</h1>
+      <h3 className={styles.tip}>Dica sobre a palavra: <span>Dica...</span>
+      </h3>
+      <div className={styles.wordContainer}>
+        <span className={styles.letter}>A</span>
+        <span className={styles.blankSquare}></span>
+      </div>
+      <div className={styles.letterContainer}>
+        <p>Tente advinhar uma letra da palavra.</p>
+        <form>
+          <input type="text" name="letter" id="letter" maxLength='1' required/>
+          <button>Advinhar</button>
+        </form>
+      </div>
+      <div className={styles.wrongLettersContainer}>
+        <p>Letras já utilizadas:</p>
+        <span>a,</span>
+        <span>b,</span>
+      </div>
     </div>
   )
 }
