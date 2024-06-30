4702882e267058c9d3098470f693bb3fd84dcfe4
Marco Cruz <marco_cruz@hotmail.com.br>
2024-06-30 10:36:10 -0300

    Escolhendo as palavras de forma aleatória

--- 06_pjSecretWord/secret_word/src/App.jsx
+++ 06_pjSecretWord/secret_word/src/App.jsx
@@ -23,7 +23,30 @@ function App() {
   const [gameStage, setGameStage] = useState(stages[0].name)
   const [words] = useState(wordList)
 
+  const [pickedWord, setPickedWord] = useState('')
+  const [pickedCategory, setPickedCategory] = useState('')
+  const [letters, setLetters] = useState([])
+
+  function pickWordAndCategory () {
+    //Escolhendo a categoria
+    const category = Object.keys(words)[Math.floor(Math.random() * Object.keys(words).length)]
+
+    //Escolhendo a Palavra
+    const word = words[category][Math.floor(Math.random() * words[category].length)]
+
+    return [ word, category ]
+  }
+
   function startGame () {
+    // Obtendo os dados
+    const [word, category] = pickWordAndCategory()
+    const wordLetters = word.toLowerCase().split('')
+
+    // Inisiando os States
+    setPickedCategory(category)
+    setPickedWord(word)
+    setLetters(wordLetters)
+
     setGameStage(stages[1].name)
   }
 
@@ -33,7 +56,7 @@ function App() {
     <div className="App">
       {gameStage === 'start' && <StartScreen startGame={startGame}/>}
       {gameStage === 'game' && <Game />}
-      {gameStage === 'end' && <GameOver />}
+      {gameStage === 'end' && <GameOver />}      
     </div>
   )
 }
--- 06_pjSecretWord/secret_word/src/components/StartScreen.jsx
+++ 06_pjSecretWord/secret_word/src/components/StartScreen.jsx
@@ -4,7 +4,6 @@ function StartScreen({func, startGame}) {
   return (
     <div className={styles.mainDiv}>
         <h1 className={styles.h1}>Palavra Secreta</h1>
-        <p>paragrafos</p>
         <button className={styles.button} onClick={startGame}>Clique aqui para começar!</button>
     </div>
   )
