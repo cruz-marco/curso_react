08469609bf668a384997e9fb0c4c1573ddb4a799
Marco Cruz <marco_cruz@hotmail.com.br>
2024-07-05 19:23:04 -0300

    Get, Post e Delete usando React Http

--- 07_req_http/req_http/src/App.jsx
+++ 07_req_http/req_http/src/App.jsx
@@ -1,15 +1,19 @@
 import { useState, useEffect } from 'react'
+import { useFetch } from './hooks/useFetch'
+import { usePost } from './hooks/usePost'
 
 import './App.css'
 
 function App() {
-  const [products, setProducts] = useState([])
-  const [name, setName] = useState('')
-  const [price, setPrice] = useState(0)
-  
   const URL = 'http://localhost:3000/products'
 
+  //const [products, setProducts] = useState(prods)
+  const [name, setName] = useState('')
+  const [price, setPrice] = useState(0)
+  const { data: prods } = useFetch(URL) // renomeando "data" para "prods"
+  
   // Resgatando dados: 
+  /*
   useEffect(() => { // não coloque um async aqui.
      // ASYNC pois é uma função assíncrona
      async function fetchData() {
@@ -23,19 +27,20 @@ function App() {
       fetchData()      
    
   }, [])
-
-  console.log(products)
+  */
 
   //Adição de produtos
   async function handleSubmit(e) {
     e.preventDefault()
 
     const product = {
-      name,
-      price
+      name: name,
+      price: Number(price)
     }
 
-    const req = await fetch(URL, { // não esquecer o await!!!!
+
+    usePost(product, URL)
+    /*const req = await fetch(URL, { // não esquecer o await!!!!
       method: 'POST',
       headers: {
         'Content-Type': 'aplication/json'
@@ -44,20 +49,31 @@ function App() {
     })    
     //3 - carregamento dinâmico
     const addedProduct = await req.json()
-    setProducts([...products, addedProduct])
+    setProducts([...products, addedProduct])*/
 
     // Limpando os inputs
-    setName('')
-    setPrice(0)
+    window.location.reload()
   }
   
+  const [delement, setDelement] = useState('');
+  function handleRemove(){
+    const url_rem = `${URL}/${delement}`
+    async function removeItem(endereco) {
+      const req = await fetch(endereco, {
+        method: 'DELETE'
+      })      
+    }
+
+    removeItem(url_rem)
+    window.Location.reload()
+  }
 
   return (
     <div className="App">
       <h1>Http com React</h1>
       <ul>
-      {products.map((product) => (
-        <li key={product.id}>Produto: {product.name} - Preço: R$:{product.price}</li>
+      {prods && prods.map((product) => (
+        <li key={product.id}>ID: {product.id} - Produto: {product.name} - Preço: R$:{product.price}</li>
       ))}
       </ul>
 
@@ -72,6 +88,18 @@ function App() {
             <input type="text" name='price' id='price' value={price} onChange={(e) => setPrice(e.target.value)}/>
           </label><br />
           <button type='submit'>enviar</button>
+        </form>
+
+        <form onSubmit={handleRemove}>
+          <label htmlFor="produtos">
+            <span>Apagar Produto:</span><br />
+            <select name="produtos" id="produtos" onChange={(e) => (setDelement(e.target.value))}>
+                {prods && prods.map((prod, i) => (
+                  <option key={i} value={prod.id}>{prod.name}</option>
+                ))}
+            </select>
+          </label><br />
+          <button type="submit">Apagar</button>
         </form>
       </div>
     </div>
---
+++ 07_req_http/req_http/src/hooks/useFetch.jsx
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react";
+
+// Custom Hook
+export function useFetch(URL) {
+    const [data, setData] = useState(null)
+
+    useEffect(() => {
+        async function receiveData() {
+            const res = await fetch(URL)
+            const json = await res.json()
+            setData(json)
+        }
+
+        receiveData()
+    }, [URL])
+
+    return { data }
+}
